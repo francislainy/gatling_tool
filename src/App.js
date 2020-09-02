2c14cb2fae51fbc75d39f67e77184d5531bf09a9
Francislainy Campos <francislainy.campos@hmhco.com>
2020-09-02 03:33:11 +0100

    Initial commit (add bootstrap for navbar, hamburger, sidebar and table)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './simple-sidebar.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="d-flex" id="wrapper">
+
+            <div className="bg-light border-right" id="sidebar-wrapper">
+                <div className="sidebar-heading">Start Bootstrap</div>
+                <div className="list-group list-group-flush">
+                    <a href="#" className="list-group-item list-group-item-action bg-light">Dashboard</a>
+                    <a href="#" className="list-group-item list-group-item-action bg-light">Shortcuts</a>
+                    <a href="#" className="list-group-item list-group-item-action bg-light">Overview</a>
+                </div>
+            </div>
+
+            <div id="page-content-wrapper">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div><a href="#" className="navbar-brand">Gatling Reporting Tool</a></div>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Dashboard</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+                <table className="table">
+                    <thead className="thead-light">
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Run Date</th>
+                        <th scope="col">Created</th>
+                        <th scope="col">Category</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                    </tr>
+                    </tbody>
+                </table>
+
+            </div>
+
+        </div>
+    )
 }
 
 export default App;
