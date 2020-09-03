fc3fa02e118a7d11e187fabb26c6ab473963d8a7
Francislainy Campos <francislainy.campos@hmhco.com>
2020-09-03 23:29:48 +0100

    Open modal from button click

--- src/App.js
+++ src/App.js
@@ -1,132 +1,112 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './simple-sidebar.css'
-import Popup from './components/Popup'
-// import Dialog from '@material-ui/core/Dialog';
-import {Dialog} from '@material-ui/core';
+import Modal from 'react-bootstrap/Modal'
+import Button from "@material-ui/core/Button";
 
-// Another test
-class App extends Component{
+function App() {
 
-    constructor(props) {
-        super(props);
-        this.state = {};
-        this.openPopupHandler = this.openPopupHandler.bind(this);
-        this.closePopupHandler = this.closePopupHandler.bind(this);
-    }
+    const [show, setShow] = useState(false);
 
-    state = {showPopup: false};
-    // state = {openDialog: false};
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-    openPopupHandler = () => {
-        this.setState({showPopup: true});
-    }
+    return (
+        <div className="d-flex" id="wrapper">
 
-    closePopupHandler = () => {
-        this.setState({showPopup: false});
-    }
-
-    // handleOpenDialog() {
-    //     this.setState({
-    //         openDialog: true
-    //     });
-    // }
-    //
-    // handleCloseDialog() {
-    //     this.setState({
-    //         openDialog: false
-    //     });
-    // }
-
-    render() {
-
-        let popup = null;
-        if(this.state.showPopup) {
-            popup = (<Popup message='the text you need to display' closeMe={this.closePopupHandler}/>);
-            // popup = (<Dialog fullScreen={false} open={this.handleOpenDialog} onCancel={this.handleCloseDialog}/>);
-        }
-
-        return (
-            <div className="d-flex" id="wrapper">
-
-                <div className="bg-light border-right" id="sidebar-wrapper">
-                    <div className="sidebar-heading">Start Bootstrap</div>
-                    <div className="list-group list-group-flush">
-                        <a href="#" className="list-group-item list-group-item-action bg-light">Dashboard</a>
-                        <a href="#" className="list-group-item list-group-item-action bg-light">Shortcuts</a>
-                        <a href="#" className="list-group-item list-group-item-action bg-light">Overview</a>
-                    </div>
+            <div className="bg-light border-right" id="sidebar-wrapper">
+                <div className="sidebar-heading">Start Bootstrap</div>
+                <div className="list-group list-group-flush">
+                    <a href="#" className="list-group-item list-group-item-action bg-light">Dashboard</a>
+                    <a href="#" className="list-group-item list-group-item-action bg-light">Shortcuts</a>
+                    <a href="#" className="list-group-item list-group-item-action bg-light">Overview</a>
                 </div>
+            </div>
 
-                <div id="page-content-wrapper">
+            <div id="page-content-wrapper">
 
-                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                        <div><a href="#" className="navbar-brand">Gatling Reporting Tool</a></div>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse"
-                                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                                aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav mr-auto">
-                                <li className="nav-item active">
-                                    <a className="nav-link" href="#">Dashboard</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-
-                    <button className="btn btn-primary"
-                            onClick={this.openPopupHandler}>
-                        Import Gatling Report
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div><a href="#" className="navbar-brand">Gatling Reporting Tool</a></div>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    {popup}
-                    {/*{this.openDialog && <Dialog open={this.handleOpenDialog} onCancel={this.handleCloseDialog}  />}*/}
 
-                    <table className="table">
-                        <thead className="thead-light">
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Run Date</th>
-                            <th scope="col">Created</th>
-                            <th scope="col">Category</th>
-                            <th scope="col">Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                        </tr>
-                        </tbody>
-                    </table>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Dashboard</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
 
-                </div>
+                <Button variant="primary" onClick={handleShow}>
+                    Import Gatling Report
+                </Button>
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Select Gatling Report Folder</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+
+
+                        Select Gatling Report Folder
+
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Cancel
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                            OK
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+
+                <table className="table">
+                    <thead className="thead-light">
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Run Date</th>
+                        <th scope="col">Created</th>
+                        <th scope="col">Category</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                    </tr>
+                    </tbody>
+                </table>
 
             </div>
-        )
 
-    }
+        </div>
+    )
 }
 
 export default App;
