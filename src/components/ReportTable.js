3e5e8b661ab45351b363d62ce7cf79290a6d3d08
Francislainy Campos <francislainy.campos@hmhco.com>
2020-11-02 03:19:57 +0000

    Delete pieces to debug useEffect issue on react table

--- src/components/ReportTable.js
+++ src/components/ReportTable.js
@@ -1,24 +1,14 @@
 import React, {useEffect, useState} from "react";
-import {Delete, Visibility} from "@material-ui/icons";
+import {Delete} from "@material-ui/icons";
 import {IconButton} from "@material-ui/core";
-import ConfirmationModal from './ConfirmationModal';
 import {deleteReport} from "../api";
 import {columns} from '../dataSource';
-import {TablePagination} from "./TablePagination";
-import DefaultColumnFilter from "./FilterInputTable";
 
-const {useHistory} = require('react-router-dom')
-const {useTable, useSortBy, usePagination, useFilters, useGlobalFilter} = require('react-table')
+const {useTable} = require('react-table')
 
-const moment = require("moment")
-const url = "http://localhost"
-const port = 8081
-
-const ReportTable = ({dataTableObj}) => {
+const MainTable = ({dataTableObj}) => {
 
     const [reports, setReports] = useState(dataTableObj.reports)
-    const [showConfirmationModal, setShowConfirmationModal] = useState(false)
-    const [idSelected, setIdSelected] = useState(0);
 
     const getInitialData = () => reports.map(report => ({
         name: report.title,
@@ -30,60 +20,17 @@ const ReportTable = ({dataTableObj}) => {
 
     const [data, setData] = useState(getInitialData)
 
-    const filterTypes = React.useMemo(
-        () => ({
-            text: (rows, id, filterValue) => {
-                return rows.filter(row => {
-                    const rowValue = row.values[id];
-                    return rowValue !== undefined
-                        ? String(rowValue)
-                            .toLowerCase()
-                            .startsWith(String(filterValue).toLowerCase())
-                        : true;
-                });
-            }
-        }),
-        []
-    );
-
-    const defaultColumn = React.useMemo(
-        () => ({
-            Filter: DefaultColumnFilter
-        }),
-        []
-    );
-
     const {
         getTableProps,
         getTableBodyProps,
-        headerGroups,
         rows,
         prepareRow,
-        page,
-        canPreviousPage,
-        canNextPage,
-        pageOptions,
-        pageCount,
-        gotoPage,
-        nextPage,
-        previousPage,
-        setPageSize,
-        state: {pageIndex, pageSize}
     } = useTable(
         {
             columns,
             data,
-            initialState: {pageIndex: 0},
-            defaultColumn,
-            filterTypes
-        },
-        useFilters,
-        useGlobalFilter,
-        useSortBy,
-        usePagination,
+        }
     );
-
-    const onHide = () => setShowConfirmationModal(false);
 
     useEffect(() => {
 
@@ -91,79 +38,33 @@ const ReportTable = ({dataTableObj}) => {
 
     }, [dataTableObj.reports]);
 
-    let history = useHistory();
+    const onConfirmDelete = (id) => {
 
-    const handleClick = (id) => {
-        history.push(`/report/${id}`);
-    }
-
-    const handleDeletePopUp = (id) => {
-
-        setIdSelected(id)
-
-        setShowConfirmationModal(true)
-    }
-
-    const onConfirmDelete = () => {
-
-        const urlAndPort = {
+        const axiosParams = {
             url: url,
             port: port,
-            id: idSelected
+            id: id
         }
 
-        deleteReport(urlAndPort).then(() => {
+        deleteReport(axiosParams).then(() => {
 
-            const del = reports.filter(report => idSelected !== report.id)
+            const del = reports.filter(report => id !== report.id)
             setReports(del)
 
-            setShowConfirmationModal(false)
         })
     }
 
-    const getDateFormatted = (dateTimeStamp) => {
-
-        const date = moment(dateTimeStamp).format('DD-MM-YYYY HH:mm:ss');
-
-        return <> {date}</>;
-    }
-
     return (
-        <div className="margin10">
-            <table {...getTableProps()} className="table table-bordered tableReport">
-                <thead className="thead-dark">
-                {headerGroups.map(headerGroup => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map(column => (
-                            <th {...column.getHeaderProps(column.getSortByToggleProps())}
-                                onClick={() => column.toggleSortBy(!column.isSortedDesc)}>
-                                {column.render('Header')}
-                                <span>
-                                    {column.isSorted ? (column.isSortedDesc ? " 🔽" : " 🔼") : ""}
-                                </span>
-                                {
-                                    column.Header !== "Actions" &&
-                                    <div>{column.canFilter ? column.render("Filter") : null}</div>
-                                }
-                            </th>
-                        ))}
-                    </tr>
-                ))}
-                </thead>
+        <div>
+            <table {...getTableProps()}>
                 <tbody {...getTableBodyProps()}>
-                {page.map((row, i) => {
+                {rows.map((row, i) => {
                     prepareRow(row)
                     return (
                         <tr {...row.getRowProps()}>
                             <td>{row.original.name}</td>
-                            <td>{getDateFormatted(row.original.runDate)}</td>
-                            <td>{getDateFormatted(row.original.createdDate)}</td>
-                            <td>{row.original.category}</td>
                             <td>
-                                <IconButton onClick={() => handleClick(row.original.actions)}>
-                                    <Visibility/>
-                                </IconButton>
-                                <IconButton onClick={() => handleDeletePopUp(row.original.actions)}>
+                                <IconButton onClick={() => onConfirmDelete(row.original.actions)}>
                                     <Delete/>
                                 </IconButton>
                             </td>
@@ -171,34 +72,23 @@ const ReportTable = ({dataTableObj}) => {
                     )
                 })}
                 </tbody>
+                <tbody>
+                {/*{reports.map((item) => {*/}
+                {/*    return [*/}
+                {/*        <tr key={item.id}>*/}
+                {/*            <td>{item.title}</td>*/}
+                {/*            <td>*/}
+                {/*                <IconButton onClick={() => onConfirmDelete(item.id)}>*/}
+                {/*                    <Delete/>*/}
+                {/*                </IconButton>*/}
+                {/*            </td>*/}
+                {/*        </tr>*/}
+                {/*    ];*/}
+                {/*})}*/}
+                </tbody>
             </table>
-            <div className="tablePagination">
-                <TablePagination onClick={() => gotoPage(0)} canPreviousPage={canPreviousPage}
-                                 onClick1={() => previousPage()} onClick2={() => nextPage()} canNextPage={canNextPage}
-                                 onClick3={() => gotoPage(pageCount - 1)} pageIndex={pageIndex}
-                                 pageOptions={pageOptions}
-                                 onChange={e => {
-                                     const page = e.target.value ? Number(e.target.value) - 1 : 0;
-                                     gotoPage(page);
-                                 }} value={pageSize} onChange1={e => {
-                    setPageSize(Number(e.target.value));
-                }} callbackfn={pageSize => (
-                    <option key={pageSize} value={pageSize}>
-                        Show {pageSize}
-                    </option>
-                )}/>
-            </div>
-            <ConfirmationModal
-                showHeader={false}
-                show={showConfirmationModal}
-                onHide={onHide}
-                onConfirm={onConfirmDelete}
-                ok={'OK'}
-                cancel={'Cancel'}
-                body={'Are you sure you want to delete this item?'}
-            />
         </div>
     )
 }
 
-export default ReportTable
+export default MainTable
